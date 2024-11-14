de247fd086bcf8a81e28fb39965106bbf33c7ea4
ramii845 <rami.elfrikha@gmail.com>
2024-11-14 20:06:19 +0100

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <head>
+       
         <title>Mon application Laravel</title>
 
 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
 <link href="{{ secure_asset('frontend/assets/index-CPubGRoS.css') }}"
 rel="stylesheet">
+</head>
 
-        
-    </head>
+
     <body >
     <div id="root"></div>
 <script src="{{ secure_asset('frontend/assets/index-CHU5ZB9Z.js')
